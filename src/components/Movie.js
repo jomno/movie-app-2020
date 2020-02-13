2c541e6db66a93137f64ee196afba83659bada69
jomno <jake8955@naver.com>
2020-02-13 19:52:16 +0900

    20200213 #6.2

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,7 +9,7 @@ function Movie({year, title, summary, poster, genres}) {
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <h5 className="movie__summary">{summary}</h5>
+        <h5 className="movie__summary">{summary.slice(0, 180)}...</h5>
         <ul className="movie__genres">
           {genres.map((genre, index) => {
             return (
